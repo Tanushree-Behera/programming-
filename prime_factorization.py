05bc56e106ac4197d42e7fac67478a9add7059a9
Tanushree Behera <119121923+Tanushree-Behera@users.noreply.github.com>
2025-08-31 21:53:00 +0530

    Create prime_factorization.py

---
+++ prime_factorization.py
@@ -0,0 +1,21 @@
+def prime_factorization(n):
+    factors = []
+    
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+        
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+        i += 2  
+    if n > 1:
+        factors.append(n)
+        
+    return factors
+
+N = 18
+output = prime_factorization(N)
+print(output)
