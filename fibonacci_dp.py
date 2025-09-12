bae2f82b44de88f0eed6c0833868296f32c3a1b9
Tanushree Behera <119121923+Tanushree-Behera@users.noreply.github.com>
2025-09-12 19:35:37 +0530

    Create fibonacci_dp.py

---
+++ fibonacci_dp.py
@@ -0,0 +1,16 @@
+def fibonacci_dp(n):
+    if n <= 1:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+n = 5
+output = fibonacci_dp(n)
+print(output)
